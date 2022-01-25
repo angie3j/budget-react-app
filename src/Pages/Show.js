60dcd219950d06fd1cbcedf8a45cf278ee773e67
Angie G <angieg@Angies-MBP.fios-router.home>
2022-01-25 00:13:08 -0500

    Show Page

---
+++ src/Pages/Show.js
@@ -0,0 +1,14 @@
+import ShowDetails from "../Components/ShowDetails"; 
+
+function Show() {
+    return (
+        <div className="ShowDetails">
+
+            <h2>Bank Account Total:</h2>
+            <ShowDetails />
+
+        </div>
+    );
+};
+
+export default Show;
