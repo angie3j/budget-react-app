a7d31ab2832f2ae07160fc0ed4a6a7ab894299fd
Angie G <angieg@Angies-MBP.fios-router.home>
2022-02-05 13:56:32 -0500

    Update API URL

--- src/Components/Transaction.js
+++ src/Components/Transaction.js
@@ -1,19 +1,50 @@
 import { Link } from "react-router-dom";
 
-function Transaction({ transaction, id}) {
+function Transaction({ transaction, index }) {
+  console.log(transaction);
   return (
-    
     <tr>
       <td>
-        {transaction.date}
+        {transaction.date ? (
+          <span>{transaction.date}</span>
+        ) : (
+          <span>&nbsp; &nbsp; &nbsp;</span>
+        )}
+      </td>
+      <td></td>
+      <td>
+        <Link to={`/transactions/${index}`}>{transaction.item_name}</Link>
       </td>
       <td>
-        <Link to={`/transactions/${id}`}>HELLO</Link>
+        {transaction.amount ? (
+          <span>{transaction.amount}</span>
+        ) : (
+          <span>&nbsp; &nbsp; &nbsp;</span>
+        )}
       </td>
-      
     </tr>
-    
   );
-};
+}
 
 export default Transaction;
+
+
+// import { Link } from "react-router-dom";
+
+// function Transaction({ transaction, id}) {
+//   return (
+    
+//     <tr>
+//       <td>
+//         {transaction.date}
+//       </td>
+//       <td>
+//         <Link to={`/transactions/${id}`}>HELLO</Link>
+//       </td>
+      
+//     </tr>
+    
+//   );
+// };
+
+// export default Transaction;
--- src/Components/Transactions.js
+++ src/Components/Transactions.js
@@ -14,7 +14,6 @@ function Transactions() {
         .get(`${URL}/transactions`)
         .then((response) =>{
             setTransactions(response.data)
-
         })
 
         .catch((error) => console.warn('catch'))
@@ -22,26 +21,52 @@ function Transactions() {
     }, [URL])
 
     const transactionsFile = transactions.map((transaction, id) => {
-        return (<div key={ id } id={ id } className='Transactions'>
-            
+        return (<div key={ id } id={ id } className='Transactions'> 
+        
             <p>{transaction.date}</p>
             <a href={`/transactions/${id}`}>{transaction.source}</a>
             <p>${transaction.amount}</p>
-        </div>)
+             </div>)
     })
+    
+
+    // let total = transactions.map((transaction) => transaction.amount);
+
+    // let amount = total.reduce((prev, curr) => Number(prev) + Number(curr));
 
     return (
         
         <div className="container">
-            
-          
-        
+            {/* <h1>Budget Total: {"$" + amount.toFixed(2)}</h1> */}
                        {transactionsFile}
-            
-            
-            
-        </div>  
+        </div> 
+        
+        
+    //      <div className="Transactions">
+    //      <section>
+    //        <table>
+    //          <thead>
+    //            <tr>
+    //              <th></th>
+    //              <th>Take me there</th>
+    //              <th>See this bookmark</th>
+    //            </tr>
+    //          </thead>
+    //          <tbody>
+
+    //            {transactions.map((transaction, id) => {
+    //                    return (<tr key={ id } id={ id } className='Transactions'>
+    //            <p>{transaction.date}</p>
+    //     <a href={`/transactions/${id}`}>{transaction.source}</a>
+    //     <p>${transaction.amount}</p>
+    //      </tr>)
+    //            })}
+    //          </tbody>
+    //        </table>
+    //      </section>
+    //    </div>
     );
 };
+
 
 export default Transactions;
